d07a9fb50a35d63f9febbb2a51772bbe9854ea4c
Jamil Gomes <jamil.lannister23@gmail.com>
2019-02-20 16:29:49 -0300

    primeiro comit

---
+++ jdbc/src/main/java/infra/ConnectionPool.java
@@ -0,0 +1,35 @@
+package infra;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+public class ConnectionPool {
+
+private DataSource dataSource;
+	
+	
+	public ConnectionPool() {
+		
+		MysqlDataSource mysqlDataSource = new MysqlDataSource();
+		String url = "jdbc:mysql://localhost/locadora?useSSL=false";
+		
+		mysqlDataSource.setURL(url);
+		mysqlDataSource.setUser("root");
+		mysqlDataSource.setPassword("");
+		
+		this.dataSource = mysqlDataSource;
+		
+	}
+	
+	
+	public Connection getConexao() throws SQLException {
+		
+		Connection conexao = dataSource.getConnection();
+	
+		return conexao;
+	}
+}
